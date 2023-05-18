01e94637b9bee0de94eda46917f9884829da0546
Hayden McSorley <haymcsorley@gmail.com>
2023-05-18 11:56:31 -0500

    Initial commit

--- lib/room.rb
+++ lib/room.rb
@@ -1,6 +1,19 @@
 class Room
+  attr_reader :category
 
-  def initialize
-
+  def initialize(category, width, length)
+    @category = category
+    @width = width
+    @length = length
+    @area = length * width
   end
+
+  def area(length, width)
+    area = 10 * 13
+    return area
+  end
+    
+
+
+    
 end
